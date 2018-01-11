a766d0f9ee0a8d7ae302cdd3cddc1dbab42eaccd
shark <shark@DESKTOP-5MGOHDH>
2018-01-11 11:13:04 -0800

    added methods to return the numerical hints for the rows and columns as a string of numbers seperated by spaces (methods: public String getColumnHints(int column) public String getRowHints(int row))

--- Nonogram/src/com/board/Board.java
+++ Nonogram/src/com/board/Board.java
@@ -16,33 +16,65 @@ public class Board {
 		}
 	}
 	
-	//create a board from file or String
+	//create a board from String
 /*	public Board(String boardName) {
 		
 	}
 */
+	
+	// start with a pre-made board
+	public Board(NonoBlock[][] board) {
+		this.board = board;
+	}
 
-	public String getLineHints(int line) {
-		for(int i=0;i<this.board[line].length;i++) {
-			
+	// returns the hint for filled blocks in a column as a string
+	public String getColumnHints(int column) {
+		int counter = 0;
+		String hint = "";
+		for(int i=0;i<this.board.length;i++) {
+			if(this.board[i][column].isFilled()) {
+				counter++;
+			}else {
+				if(counter>0)
+					hint+=counter+" ";
+				counter=0;
+			}	
 		}
-		return "";
+		if(counter>0)
+			hint+=counter+" ";
+		return hint.trim();
+	}
+	
+	// returns the hint for filled blocks in a row as a string
+	public String getRowHints(int row) {
+		int counter = 0;
+		String hint = "";
+		for(int i=0;i<this.board[row].length;i++) {
+			if(this.board[row][i].isFilled()) {
+				counter++;
+			}else {
+				if(counter>0)
+					hint+=counter+" ";
+				counter=0;
+			}	
+		}
+		if(counter>0)
+			hint+=counter+" ";
+		return hint.trim();
 	}
 	
 	//returns true if solved and false otherwise
-	public boolean isSolved() {
+	public boolean isBoardSolved() {
 		for(int i=0;i<board.length; i++) {
 			for(int j=0;j<board[i].length; j++) {
-				if(!(board[i][j].isCorrect())) {
-					return false;
-				}
+				if(!(board[i][j].isCorrect())) return false; //if there is an incorrect block then the Nonogram isn't solved
 			}
 		}
 		return true;
 	}
 
 	//returns true if square is solved and false otherwise
-	public boolean isBlockSolved(int y, int x) {
+	public boolean isBlockSolved(int x, int y) {
 		return board[x][y].isCorrect();
 	}
 	
