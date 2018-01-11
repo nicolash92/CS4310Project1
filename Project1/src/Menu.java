b19043cc1813b7d131dfac0b94be768c3d1e8927
DNM <DNM@10.248.155.19>
2018-01-10 16:06:18 -0800

    Menu class

---
+++ Project1/src/Menu.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+import java.lang.Exception;
+import java.lang.String;
+
+public class Menu {
+	private int width;
+	private int height;
+	
+	public Menu() {
+		width = height = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Welcome to Nonogram"); 
+		System.out.println("[1] New Game"); 
+		System.out.println("[2] Constructor"); 
+		
+		System.out.print("Insert Selection: ");
+		selection(sc.nextInt());	
+		
+		sc.close();
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	//Selection on menu
+	public void selection(int select) {
+		System.out.println();
+		
+		switch(select) {
+		case 1:
+			userLoadGame();
+			break;
+			
+		case 2:
+			constructor();
+			break;
+			
+		default:
+			System.out.println("The selection is invalid");	
+		}
+	}
+	
+	public void constructor() {
+		Scanner sc = new Scanner(System.in);
+		int width = height = -1;
+		
+		System.out.println("Level Constructor (MAX = 10)");
+		
+		System.out.print("Width: ");
+		setWidth(sc.nextInt());
+		
+		System.out.print("Height: ");
+		setHeight(sc.nextInt());
+		
+		sc.close();
+	}
+	
+	public void userLoadGame() {
+		//text file 
+		
+		System.out.println("Select Level");
+	}
+	
+	
+}
